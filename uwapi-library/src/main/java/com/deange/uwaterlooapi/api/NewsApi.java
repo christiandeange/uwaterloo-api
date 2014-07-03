0b183ad9074079f4b3cc1c0bab8e3696cf19bfa7
cdeange <christiandeange@gmail.com>
2014-07-03 16:00:02 -0700

    Add documentation for NewsApi.getNews()

--- uwapi-library/src/main/java/com/deange/uwaterlooapi/api/NewsApi.java
+++ uwapi-library/src/main/java/com/deange/uwaterlooapi/api/NewsApi.java
@@ -6,6 +6,10 @@ import retrofit.http.GET;
 
 public interface NewsApi {
 
+    /**
+     * This method returns a list of the freshest (most recently updated) 100
+     * University of Waterloo news items as crawled from all University WCMS sites
+     */
     @GET("/news.{format}")
     public Response.News getNews();
 
