efd763dd63d8b3cc5001678c8a4a0822152c2e3c
Christian De Angelis <christiandeange@gmail.com>
2017-03-22 00:58:08 -0700

    Reimport Gotham fonts

--- sample/src/main/assets/fonts/README.md
+++ sample/src/main/assets/fonts/README.md
@@ -3,13 +3,19 @@ Missing Fonts
 
 **The following proprietary fonts must be manually imported into this folder:**
 
-- Gotham-Light.otf
-- Gotham-LightItalic.otf
-- Gotham-Book.otf
-- Gotham-BookItalic.otf
-- Gotham-Medium.otf
-- Gotham-MediumItalic.otf
-- Gotham-Bold.otf
-- Gotham-BoldItalic.otf
+- Gotham-Thin.ttf
+- Gotham-ThinItalic.ttf
+- Gotham-Light.ttf
+- Gotham-LightItalic.ttf
+- Gotham-Book.ttf
+- Gotham-BookItalic.ttf
+- Gotham-Medium.ttf
+- Gotham-MediumItalic.ttf
+- Gotham-Bold.ttf
+- Gotham-BoldItalic.ttf
+- Gotham-Black.ttf
+- Gotham-BlackItalic.ttf
+- Gotham-Ultra.ttf
+- Gotham-UltraItalic.ttf
 
 Alternatively, you can disable Calligraphy in [MainApplication.java](https://github.com/cdeange/uwaterloo-api/blob/master/uwaterlooapi/src/main/java/com/deange/uwaterlooapi/sample/MainApplication.java).
--- sample/src/main/java/com/deange/uwaterlooapi/sample/utils/FontUtils.java
+++ sample/src/main/java/com/deange/uwaterlooapi/sample/utils/FontUtils.java
@@ -17,15 +17,21 @@ import uk.co.chrisjenx.calligraphy.TypefaceUtils;
 
 public final class FontUtils {
 
-    public static final int LIGHT = 0;
-    public static final int LIGHT_ITALIC = 1;
+    public static final int THIN = 0;
+    public static final int LIGHT = 1;
     public static final int BOOK = 2;
-    public static final int BOOK_ITALIC = 3;
-    public static final int MEDIUM = 4;
-    public static final int MEDIUM_ITALIC = 5;
-    public static final int BOLD = 6;
-    public static final int BOLD_ITALIC = 7;
-    public static final int FANCY = 8;
+    public static final int MEDIUM = 3;
+    public static final int BOLD = 4;
+    public static final int BLACK = 5;
+    public static final int ULTRA = 6;
+    public static final int THIN_ITALIC = 7;
+    public static final int LIGHT_ITALIC = 8;
+    public static final int BOOK_ITALIC = 9;
+    public static final int MEDIUM_ITALIC = 10;
+    public static final int BOLD_ITALIC = 11;
+    public static final int BLACK_ITALIC = 12;
+    public static final int ULTRA_ITALIC = 13;
+    public static final int FANCY = 14;
 
     public static final int DEFAULT = BOOK;
 
@@ -39,14 +45,20 @@ public final class FontUtils {
     public static void init(final Context context) {
         sAssets = context.getResources().getAssets();
 
+        FONTS.put(THIN, context.getString(R.string.font_thin));
         FONTS.put(LIGHT, context.getString(R.string.font_light));
-        FONTS.put(LIGHT_ITALIC, context.getString(R.string.font_light_italic));
         FONTS.put(BOOK, context.getString(R.string.font_book));
-        FONTS.put(BOOK_ITALIC, context.getString(R.string.font_book_italic));
         FONTS.put(MEDIUM, context.getString(R.string.font_medium));
-        FONTS.put(MEDIUM_ITALIC, context.getString(R.string.font_medium_italic));
         FONTS.put(BOLD, context.getString(R.string.font_bold));
+        FONTS.put(BLACK, context.getString(R.string.font_black));
+        FONTS.put(ULTRA, context.getString(R.string.font_ultra));
+        FONTS.put(THIN_ITALIC, context.getString(R.string.font_thin_italic));
+        FONTS.put(LIGHT_ITALIC, context.getString(R.string.font_light_italic));
+        FONTS.put(BOOK_ITALIC, context.getString(R.string.font_book_italic));
+        FONTS.put(MEDIUM_ITALIC, context.getString(R.string.font_medium_italic));
         FONTS.put(BOLD_ITALIC, context.getString(R.string.font_bold_italic));
+        FONTS.put(BLACK_ITALIC, context.getString(R.string.font_black_italic));
+        FONTS.put(ULTRA_ITALIC, context.getString(R.string.font_ultra_italic));
         FONTS.put(FANCY, context.getString(R.string.font_fancy));
 
         CalligraphyConfig.initDefault(new CalligraphyConfig.Builder()
@@ -78,7 +90,11 @@ public final class FontUtils {
         return TypefaceUtils.load(sAssets, fontPath);
     }
 
-    @IntDef({LIGHT, LIGHT_ITALIC, BOOK, BOOK_ITALIC, MEDIUM, MEDIUM_ITALIC, BOLD, BOLD_ITALIC, FANCY, })
-    public @interface Font { }
+    @IntDef({
+            THIN, LIGHT, BOOK, MEDIUM, BOLD, BLACK, ULTRA,
+            THIN_ITALIC, LIGHT_ITALIC, BOOK_ITALIC, MEDIUM_ITALIC, BOLD_ITALIC, BLACK_ITALIC, ULTRA_ITALIC,
+            FANCY,})
+    public @interface Font {
+    }
 
 }
